9be0bc8754b08318b820d6b1e7e2ced2cf3afc1e
Seth Rivers <davmrive@ut.utm.edu>
2024-12-09 22:03:57 -0600

    Added some more documentation

--- deque.cpp
+++ deque.cpp
@@ -10,16 +10,10 @@
 #include "deque.h"
 #include <iostream>
 
-// -------------------------------------------
-// Constructor: Initializes an empty deque
-// -------------------------------------------
 Deque::Deque() : blockmap(nullptr), numBlocks(0), blockSize(4), frontIndex(0), backIndex(0), currentSize(0) {
     allocateBlock();  // Allocate an initial block to start with
 }
 
-// -------------------------------------------
-// Destructor: Frees all dynamically allocated memory
-// -------------------------------------------
 Deque::~Deque() {
     if (blockmap != nullptr) {
         // Iterate through all the blocks and delete each dynamically allocated array
@@ -35,9 +29,6 @@ Deque::~Deque() {
     }
 }
 
-// -------------------------------------------
-// Allocates an initial block for the deque
-// -------------------------------------------
 void Deque::allocateBlock() {
     numBlocks = 1;                     // Start with one block
     blockmap = new int*[numBlocks];    // Allocate space for the blockmap (array of pointers)
@@ -47,9 +38,6 @@ void Deque::allocateBlock() {
     frontIndex = backIndex = blockSize / 2;
 }
 
-// -------------------------------------------
-// Push a value to the front of the deque
-// -------------------------------------------
 void Deque::push_front(int value) {
     // If frontIndex is out of bounds, expand the blockmap to allocate more space
     if (frontIndex < 0) {
@@ -61,9 +49,6 @@ void Deque::push_front(int value) {
     currentSize++;
 }
 
-// -------------------------------------------
-// Push a value to the back of the deque
-// -------------------------------------------
 void Deque::push_back(int value) {
     // If backIndex exceeds the block size, expand the blockmap to allocate more space
     if (backIndex >= blockSize) {
@@ -75,9 +60,6 @@ void Deque::push_back(int value) {
     currentSize++;
 }
 
-// -------------------------------------------
-// Pop a value from the front of the deque
-// -------------------------------------------
 void Deque::pop_front() {
     if (currentSize == 0) {
         // Error handling: Attempt to pop from an empty deque
@@ -90,9 +72,6 @@ void Deque::pop_front() {
     currentSize--;
 }
 
-// -------------------------------------------
-// Pop a value from the back of the deque
-// -------------------------------------------
 void Deque::pop_back() {
     if (currentSize == 0) {
         // Error handling: Attempt to pop from an empty deque
@@ -105,23 +84,14 @@ void Deque::pop_back() {
     currentSize--;
 }
 
-// -------------------------------------------
-// Check if the deque is empty
-// -------------------------------------------
 bool Deque::empty() const {
     return currentSize == 0;  // Returns true if the current size is zero
 }
 
-// -------------------------------------------
-// Get the current size of the deque
-// -------------------------------------------
 int Deque::size() const {
     return currentSize;  // Return the number of elements currently in the deque
 }
 
-// -------------------------------------------
-// Expand the blockmap when more space is needed (basic implementation)
-// -------------------------------------------
 void Deque::expandBlockmap() {
     // Create a new blockmap with one more block than the current blockmap
     int** newBlockmap = new int*[numBlocks + 1];
@@ -144,8 +114,6 @@ void Deque::expandBlockmap() {
     frontIndex = blockSize / 2;
     backIndex = frontIndex + 1;
 }
-
-
 
 
 
